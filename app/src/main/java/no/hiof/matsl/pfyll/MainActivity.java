b0729fcd04e6ab0f370cc75763e6799b04128324
Kim Roar Stenvard Dehli <kim.r.dehli@hiof.no>
2018-10-05 13:45:13 +0200

    Actual initial commit

---
+++ app/src/main/java/no/hiof/matsl/pfyll/MainActivity.java
@@ -0,0 +1,14 @@
+package no.hiof.matsl.pfyll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Adding comment to test Git integration
+    }
+}
