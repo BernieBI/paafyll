cfc4c8a3e212c8b544726df230ee68665b28e237
Mats Løvstrand Berntsen <matslovstrandberntsen@outlook.com>
2018-10-19 19:29:59 +0200

    Remove productsActivity. Added product list to fragment

--- app/src/main/java/no/hiof/matsl/pfyll/model/FragmentProducts.java
+++ app/src/main/java/no/hiof/matsl/pfyll/model/FragmentProducts.java
@@ -1,22 +1,40 @@
 package no.hiof.matsl.pfyll.model;
 
-import android.content.Intent;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
+import android.arch.paging.LivePagedListBuilder;
+import android.arch.paging.PagedList;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
 
-import no.hiof.matsl.pfyll.ProductsActivity;
+import com.google.firebase.database.FirebaseDatabase;
+
 import no.hiof.matsl.pfyll.R;
+import no.hiof.matsl.pfyll.adapter.ProductDataSourceFactory;
+import no.hiof.matsl.pfyll.adapter.ProductRecycleViewAdapter;
 
 public class FragmentProducts extends Fragment {
+    private LiveData<PagedList<Product>> products;
+    private RecyclerView recyclerView;
+    private ImageButton layoutButton;
+    private ProductRecycleViewAdapter productAdapter;
+    private int layoutColumns = 2;
+    private GridLayoutManager gridLayoutManager;
+    //firebase
+    final private FirebaseDatabase database = FirebaseDatabase.getInstance();
+
     View view;
     String TAG = "MainActivity";
+
     public FragmentProducts(){
 
     }
@@ -25,7 +43,7 @@ public class FragmentProducts extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_products, container, false);
-        Button productsButton = view.findViewById(R.id.viewProductsBtn);
+        /*Button productsButton = view.findViewById(R.id.viewProductsBtn);
 
         //Temporary button for starting ProductsActivity
         productsButton.setOnClickListener(new View.OnClickListener() {
@@ -35,10 +53,59 @@ public class FragmentProducts extends Fragment {
                 Intent productActivityIntent = new Intent(FragmentProducts.this.getActivity(), ProductsActivity.class);
                 getActivity().startActivity(productActivityIntent);
             }
-        });
+        });*/
+        Log.d(TAG, "onCreate: Started");
 
+
+        PagedList.Config config = new PagedList.Config.Builder().setPageSize(6).build();
+
+        ProductDataSourceFactory factory = new ProductDataSourceFactory(database.getReference());
+
+        products = new LivePagedListBuilder<>(factory, config).build();
+
+        layoutButton = view.findViewById(R.id.layoutButton);
+        layoutButton.setOnClickListener(layoutSwitchListener);
+
+        initRecyclerView();
         return view;
 
 
+    }
+    private void initRecyclerView(){
+
+        recyclerView = view.findViewById(R.id.product_recycler_view);
+        passProductsToView(layoutColumns);
+
+        products.observe(this, new Observer<PagedList<Product>>() {
+            @Override
+            public void onChanged(@Nullable PagedList<Product> products) {
+                productAdapter.submitList(products);
+            }
+        });
+
+    }
+
+    private View.OnClickListener layoutSwitchListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(final View v) {
+            if (layoutColumns == 2){
+                layoutColumns = 1;
+                layoutButton.setImageDrawable(getActivity().getDrawable(R.drawable.grid));
+                productAdapter.changeLayout(true);
+            }else{
+                layoutColumns = 2;
+                layoutButton.setImageDrawable(getActivity().getDrawable(R.drawable.list));
+                productAdapter.changeLayout(false);
+            }
+            gridLayoutManager.setSpanCount(layoutColumns);
+        }
+    };
+    public void passProductsToView (int layoutColumns){
+
+        productAdapter = new ProductRecycleViewAdapter(getActivity());
+        recyclerView.setAdapter(productAdapter);
+        gridLayoutManager = new GridLayoutManager(getActivity(), layoutColumns); // (Context context, int spanCount)
+        recyclerView.setLayoutManager(gridLayoutManager);
     }
 }
