74f44d2bb4032b558552b1c0073aa1a3ba303fd1
Unknown <Herman-strand@hotmail.com>
2018-10-19 18:56:26 +0200

    Added Tablayout and Fragments for each tab

--- app/src/main/java/no/hiof/matsl/pfyll/MainActivity.java
+++ app/src/main/java/no/hiof/matsl/pfyll/MainActivity.java
@@ -1,31 +1,45 @@
 package no.hiof.matsl.pfyll;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import no.hiof.matsl.pfyll.model.FragmentDrinklog;
+import no.hiof.matsl.pfyll.model.FragmentFavorites;
+import no.hiof.matsl.pfyll.model.FragmentMyActivity;
+import no.hiof.matsl.pfyll.model.FragmentProducts;
+import no.hiof.matsl.pfyll.model.ViewPagerAdapter;
+
 public class MainActivity extends AppCompatActivity {
-    String TAG = "MainActivity";
+
+    private TabLayout tabLayout;
+    private AppBarLayout appBarLayout;
+    private ViewPager viewPager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Temporary button for starting ProductsActivity
-        Button productsButton = findViewById(R.id.viewProductsBtn);
-
-        productsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: ProductsActivity started");
-                Intent productActivityIntent = new Intent(MainActivity.this, ProductsActivity.class);
-                startActivity(productActivityIntent);
-            }
-        });
+        tabLayout = findViewById(R.id.tabLayout);
+        appBarLayout = findViewById(R.id.appBarid);
+        viewPager = findViewById(R.id.viewPagerid);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        //Adding fragments
+        adapter.AddFragment(new FragmentProducts(),"Products");
+        adapter.AddFragment(new FragmentFavorites(),"Favorites");
+        adapter.AddFragment(new FragmentMyActivity(),"My activity");
+        adapter.AddFragment(new FragmentDrinklog(),"Drink log");
+        //Adapter setup
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
+
 }
