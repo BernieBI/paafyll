2f215dcc9fb8c739fbcc008364d8c689545f70f3
Kim Roar Stenvard Dehli <kim.r.dehli@hiof.no>
2018-11-20 13:55:56 +0100

    WIP

--- app/src/main/java/no/hiof/matsl/pfyll/adapter/ProductDataSource.java
+++ app/src/main/java/no/hiof/matsl/pfyll/adapter/ProductDataSource.java
@@ -96,7 +96,7 @@ public class ProductDataSource extends ItemKeyedDataSource<DocumentSnapshot, Fir
         }
     }
 
-    private Query addFilters(Query query) {
+    private Query addFilters(Query query, int loadSize) {
         String rangedField = "";
         if (filters != null) {
             for (Filter filter : filters) {
@@ -108,26 +108,38 @@ public class ProductDataSource extends ItemKeyedDataSource<DocumentSnapshot, Fir
                     case GREATER_THAN:
                         if (!rangedField.equals("") && !rangedField.equals(filter.getFieldName()))
                             break;
-                        query = query.whereGreaterThan(filter.getFieldName(), filter.getValue());
-                        rangedField = filter.getFieldName();
+                        if (rangedField.equals("")) {
+                            rangedField = filter.getFieldName();
+                            query = query.orderBy(rangedField);
+                        }
+                        query = query.startAfter(filter.getValue());
                         break;
                     case GREATER_THAN_OR_EQUALS:
                         if (!rangedField.equals("") && !rangedField.equals(filter.getFieldName()))
                             break;
-                        query = query.whereGreaterThanOrEqualTo(filter.getFieldName(), filter.getValue());
-                        rangedField = filter.getFieldName();
+                        if (rangedField.equals("")) {
+                            rangedField = filter.getFieldName();
+                            query = query.orderBy(rangedField);
+                        }
+                        query = query.startAt(filter.getValue());
                         break;
                     case LESS_THAN:
                         if (!rangedField.equals("") && !rangedField.equals(filter.getFieldName()))
                             break;
-                        query = query.whereLessThan(filter.getFieldName(), filter.getValue());
-                        rangedField = filter.getFieldName();
+                        if (rangedField.equals("")) {
+                            rangedField = filter.getFieldName();
+                            query = query.orderBy(rangedField);
+                        }
+                        query = query.endBefore(filter.getValue());
                         break;
                     case LESS_THAN_OR_EQUALS:
                         if (!rangedField.equals("") && !rangedField.equals(filter.getFieldName()))
                             break;
-                        query = query.whereLessThanOrEqualTo(filter.getFieldName(), filter.getValue());
-                        rangedField = filter.getFieldName();
+                        if (rangedField.equals("")) {
+                            rangedField = filter.getFieldName();
+                            query = query.orderBy(rangedField);
+                        }
+                        query = query.endAt(filter.getValue());
                         break;
                     case LIKE:
                         if (!rangedField.equals("") && !rangedField.equals(filter.getFieldName()))
@@ -142,7 +154,7 @@ public class ProductDataSource extends ItemKeyedDataSource<DocumentSnapshot, Fir
                 }
             }
         }
-        return query;
+        return query.limit(loadSize);
     }
 
     // TODO: Refactor to remove duplicate code
@@ -157,8 +169,7 @@ public class ProductDataSource extends ItemKeyedDataSource<DocumentSnapshot, Fir
                 query = collection.startAfter(key);
         } else
             query = collection;
-        query = addFilters(query);
-        query = query.limit(loadSize);
+        query = addFilters(query, loadSize);
 
         query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
 
@@ -180,17 +191,15 @@ public class ProductDataSource extends ItemKeyedDataSource<DocumentSnapshot, Fir
     private void loadDataByValueFilter(@NonNull final TaskCompletionSource<List<FirestoreProduct>> taskCompletionSource, DocumentSnapshot key, int loadSize, boolean reverse) {
         CollectionReference collection = database.collection(COLLECTION_PATH);
 
-        Query query;
+        Query query = collection;
         if (key != null) {
             if (reverse)
-                query = collection.endBefore(key);
+                query = query.endBefore(key);
             else
-                query = collection.startAfter(key);
-        } else
-            query = collection;
+                query = query.startAfter(key);
+        }
 
-        query = addFilters(query);
-        query = query.limit(loadSize);
+        query = addFilters(query, loadSize);
 
         query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
             @Override
